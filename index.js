38bf253dcb2604f5d4ab3bf1819e5b285713b1f5
Emmi Lindsey <emmilindsey@gmail.com>
2023-10-31 12:59:19 -0600

    array find method lab - not all tests passing but working in replit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+  
+  function superbowlWin(myArray) {
+  
+    function aWin (element, index, myArray) {
+        return (myArray[index].result === "W")
+      }
+  
+      const didTheyWin = ((record.find(aWin)));
+  
+    if (didTheyWin) {
+        console.log(didTheyWin.year)
+    } else {
+        console.log("undefined")
+    }
+      
+  }
+
+  superbowlWin(record)
